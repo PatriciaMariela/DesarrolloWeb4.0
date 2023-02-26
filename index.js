e75e0583b69d821398f5e056257aa4135a9c51ec
PaBernasconi <pachuberna@gmail.com>
2023-02-26 19:27:10 -0300

    Curso UTN Intro al desarrollo web

---
+++ index.js
@@ -0,0 +1,33 @@
+//1.Declarar una variable de nombre “auto”, asignarle el valor “Corolla” y mostrarla por
+//consola*.
+var nombre = 'Corolla'
+concosle.log(nombre)
+typeof(nombre)
+
+//2. Asignarle el valor “308” a la variable declarada en el ejercicio anterior y mostrarla por
+//consola.
+nombre = 308
+concosle.log(nombre)
+typeof(nombre)
+
+//3. A la misma variable de los ejercicios anteriores, asignarle el valor “C4”. Volver a
+//mostrarla por consola.
+nombre = 'C4'
+concosle.log(nombre)
+typeof(nombre)
+
+//4. Declarar dos variables “nombre” y “apellido”, asignarles tu nombre y apellido. Luego
+//mostrar por consola ambos valores. Usar un solo console.log( )
+var nombre = 'Patricia'
+var apellido = 'Bernasconi'
+concosle.log(nombre + apellido)
+
+//5. Dada una variable llamada “fruta” con el valor “naranja”. Declarar una variable
+//“tipoDeLaVariableFruta” y asignarle como valor el tipo de la variable “fruta”. **
+fruta = 'naranja'
+var tipoDeLaVariableFruta = fruta
+
+
+//6. Para los ejercicios 1, 2 y 3, mostrar por consola el tipo de la variable en cada nueva
+//asignación.
+typeof(nombre)
